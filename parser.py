335f42a139bf367e7d46ee2ca788f70fa48ebf2f
Igor20099 <megakill123@yandex.ru>
2018-07-18 16:54:12 +0500

    HTMLParser v0.3: Add method parse

--- parser.py
+++ parser.py
@@ -11,23 +11,55 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 #Настройки экрана
-Config.set('graphics','width',640)
+Config.set('graphics','width',320)
 Config.set('graphics','height',480)
 Config.set('graphics','resizable',0)
 
 
 class ParserApp(App):
+    def parse(self,url_input):
+            #TODO:Добавить алгортм парсинга
+            self.url = requests.get(self.url_input.text)
+            self.b = bs4.BeautifulSoup(self.url.text,'html.parser')
+            self.tag = self.tag_input.text
+            self.p = self.b.select(self.tag)
+            self.text = self.p[0].getText()
+            self.filename = self.filename_input.text
+            with open(self.filename + '.txt','w') as f:
+                f.write(self.text)
     def build(self):
-        def parse(self):
-            pass
-        self.bl = BoxLayout()
-        self.url_label = Label(text = 'URL')
-        self.tag_label = Label(text ='Tag,class,id')
-        self.filename_label = Label(text = 'Filename')
+        self.bl = BoxLayout(orientation = 'vertical')
+        self.url_bl = BoxLayout(orientation = 'horizontal')
+        self.tag_bl = BoxLayout(orientation = 'horizontal')
+        self.filename_bl = BoxLayout(orientation = 'horizontal')
+        self.url_label = Label(text = 'URL',size_hint = (0.4,1))
+        self.tag_label = Label(text ='Tag,class,id',size_hint = (0.4,1))
+        self.filename_label = Label(text = 'Filename',size_hint = (0.4,1))
         self.url_input = TextInput()
         self.tag_input = TextInput()
         self.filename_input = TextInput()
-        self.btn_parse = Button(text = 'Parse')
+
+        self.url_bl.add_widget(self.url_label)
+        self.url_bl.add_widget(self.url_input)
+        self.bl.add_widget(self.url_bl)
+        
+        
+
+
+        self.btn_parse = Button(text = 'Parse',on_press = self.parse)
+       
+       
+
+        self.tag_bl.add_widget(self.tag_label)
+        self.tag_bl.add_widget(self.tag_input)
+        self.bl.add_widget(self.tag_bl)
+
+        self.filename_bl.add_widget(self.filename_label)
+        self.filename_bl.add_widget(self.filename_input)
+        self.bl.add_widget(self.filename_bl)
+        self.bl.add_widget(self.btn_parse)
+        
+
         return self.bl
 
 
