2aac7aa83c2e250c431a352f1c2fd66f55603212
Igor20099 <megakill123@yandex.ru>
2018-07-18 18:10:01 +0500

    Change size screen

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from kivy.config import Config
 class Conf:
     def __init__(self):
         #Настройки экрана
-        Config.set('graphics','width',320)
-        Config.set('graphics','height',480)
+        Config.set('graphics','width',800)
+        Config.set('graphics','height',120)
         Config.set('graphics','resizable',0)
